f3dcb3f54be9c19367433dd89a703f43d7dc8ede
ulykpan <ulykpan@kali.lan>
2024-07-14 14:46:33 +0300

    Add a 'Switched to HashRouter'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './center/Home';
 import About from './center/About'; 
 import Servis from './center/Servis';
